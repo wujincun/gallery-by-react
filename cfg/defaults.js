d2d70a0592f9f67c1a1141039ab4650d4e745da2
wujincun <1769453779@qq.com>
2016-06-15 18:08:48 +0800

    postCss

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -12,15 +12,18 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
+        loader:'style-loader!css-loader!postcss-loader'
+        // 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
       },
       {
         test: /\.sass/,
         loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+        //indentedSyntax后面的querystring是指sass能用类ruby的形式写
       },
       {
         test: /\.scss/,
         loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+        //'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
--- cfg/dist.js
+++ cfg/dist.js
@@ -14,17 +14,17 @@ let config = Object.assign({}, baseConfig, {
   cache: false,
   devtool: 'sourcemap',
   plugins: [
-    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.DedupePlugin(),//检测并除掉相似的文件或者冗余的内容
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
     }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),//压缩输出js代码
+    new webpack.optimize.OccurenceOrderPlugin(),//按引用的频率来排序各模块，引用越频繁，ID越短，减小文件大小
+    new webpack.optimize.AggressiveMergingPlugin(),//优化生成的代码段，合并相似的trank，提取公共部分
+    new webpack.NoErrorsPlugin()//保证编译过程不出错
   ],
   module: defaultSettings.getDefaultModules()
 });
